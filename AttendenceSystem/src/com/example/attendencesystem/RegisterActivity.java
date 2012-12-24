e7accfbfd7c28342361d0d2d9ceaeec03cec0414
awoiyume <awoiyume@gamil.com>
2012-12-24 23:56:54 +0800

    第一次提交

---
+++ AttendenceSystem/src/com/example/attendencesystem/RegisterActivity.java
@@ -0,0 +1,21 @@
+package com.example.attendencesystem;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RegisterActivity extends Activity{
+	
+	
+}
